e919b43848b195d765341b60701ecc2ff368c249
Foyjur Rafee <frnaib9@gmail.com>
2026-01-20 14:23:03 +0600

    files added

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.photos').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    pauseOnHover: false
+  });
+});
