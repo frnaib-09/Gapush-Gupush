c874c9d52a7edf655b1cf83dab07920bac704a80
MR HAMEM <hamemapple2018@gmail.com>
2026-01-20 22:41:41 +0600

    added the css and others files

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Scroll Animation
+const revealElements = document.querySelectorAll('.reveal');
+
+const revealObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
+        revealObserver.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.15
+  }
+);
+revealElements.forEach(el => revealObserver.observe(el));
